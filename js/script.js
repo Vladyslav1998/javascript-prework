0670ebad15207324aefab830ba2264fd80ffc8cc
Vladyslav Mazurenko <vladmazurenko13798@gmail.com>
2022-01-14 20:28:59 +0100

    Add calculation function in js

--- js/script.js
+++ js/script.js
@@ -9,6 +9,20 @@ function clearMessages() {
 }
 
 let computerMove = 'kamień';
+
 let playerMove = 'papier';
+
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let randomFraction = Math.random();
+
+printMessage('Wylosowany ułamek to: ' + randomFraction);
+
+let calculation = randomFraction * 3 + 1;
+
+printMessage('Ułamek to: ' + calculation);
+
+let randomNumber = Math.floor(calculation);
+
+printMessage('Cały ułamek to: ' + randomNumber);
 
